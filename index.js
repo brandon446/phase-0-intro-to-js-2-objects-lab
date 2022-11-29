443fdefdbc23565c5c557a5c457a275459353fb4
brandon <brandon.ngongolo@student.moringaschool.com>
2022-11-29 10:33:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+const employee ={
+    name:"Shanice",
+    streetAddress:"11 Kilimani"
+
+}
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    return{...employee, ...{[key]:value}};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    let employeeNew =Object.assign({}, employee);
+    delete employeeNew[key];
+    return employeeNew;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
